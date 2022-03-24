37dca9e262e88e7923864987731bcf0075afd3bb
Yeison Ramirez <yeisonr2020@gmail.com>
2022-03-23 23:14:55 -0500

    commit front

---
+++ src/components/StudentTableRow.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+
+export default class StudentTableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteStudent = this.deleteStudent.bind(this);
+    }
+    deleteStudent() {
+        axios.delete('http://localhost:4001/students/delete-student/' + this.props.obj._id)
+            .then((res) => {
+                console.log('Student successfully deleted!')
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+    render() {
+        return (
+            <tr>
+                <td>{this.props.obj.name}</td>
+                <td>{this.props.obj.email}</td>
+                <td>{this.props.obj.rollno}</td>
+                <td>{this.props.obj.phone}</td>
+                <td>
+                    <Link className="edit-link" to={"/edit-student/" + this.props.obj._id}>
+                        Edit
+                    </Link>
+                    <Button onClick={this.deleteStudent} size="sm" variant="danger">Delete</Button>
+                </td>
+            </tr>
+        );
+    }
+}
